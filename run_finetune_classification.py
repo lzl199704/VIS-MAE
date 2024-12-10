51b35e6d2e98d148fc7c21c07cde2b8da0723069
LZL <106784487+lzl199704@users.noreply.github.com>
2024-12-10 12:40:23 -0500

    Update run_finetune_classification.py

--- run_finetune_classification.py
+++ run_finetune_classification.py
@@ -12,7 +12,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import Dataset
 import utils.misc as misc
-from utils.misc_update import NativeScalerWithGradNormCount as NativeScaler
+from utils.misc import NativeScalerWithGradNormCount as NativeScaler
 import swin_class
 from torchvision.transforms import functional as TF
 import utils_f
@@ -73,7 +73,10 @@ def get_args_parser():
     parser.add_argument('--batch_size', default=96, type=int)
     parser.add_argument('--transform', action='store_true')
     parser.add_argument('--num_classes', default=2, type=int)
+    parser.add_argument('--datapercent', default='', type=str)
     parser.add_argument('--epochs', default=400, type=int)
+    
+    parser.add_argument('--gpu_num', default=1, type=int)
     parser.add_argument('--save_freq', default=1, type=int)
     parser.add_argument('--checkpoint_encoder', default='', type=str)
     parser.add_argument('--checkpoint_decoder', default='', type=str)
@@ -96,6 +99,7 @@ def get_args_parser():
 
     # optimizer parameters
     parser.add_argument('--accum_iter', default=1, type=int)
+    parser.add_argument('--optimizer', default='adamw', type=str)
     parser.add_argument('--weight_decay', type=float, default=0.05,
                         help='weight decay (default: 0.05)')
     parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
@@ -110,8 +114,8 @@ def get_args_parser():
     parser.add_argument('--fold_num', default='0', type=str)
     parser.add_argument('--output_dir', default='./output_dir',
                         help='path where to save, empty for no saving')
-    parser.add_argument('--log_dir', default='./output_dir',
-                        help='path where to tensorboard log')
+    #parser.add_argument('--log_dir', default='./output_dir',
+    #                    help='path where to tensorboard log')
     parser.add_argument('--eval', action='store_true',
                         help='Perform evaluation only')
     parser.add_argument('--device', default='cuda',
@@ -127,6 +131,7 @@ def get_args_parser():
     parser.set_defaults(pin_mem=True)
     
     # distributed training parameters
+    parser.add_argument('--distributed', action='store_true')
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--local_rank', default=-1, type=int)
@@ -137,84 +142,82 @@ def get_args_parser():
 
 
 def convert_label(input_str):
-    if input_str == 'positive' or input_str == 'yes':
+    ## return value if it is not a string
+    if isinstance(input_str, str)!=True:
+        return input_str
+    
+    if input_str == 'positive' or input_str == 'yes' or input_str == 'malignant':
         return 1
     else:
         return 0
 
-        
+
 class ClassificationCSVDataset(Dataset):
-    def __init__(self,task, root_dir, image_size,fold_num, split="train",add_transform=True):
-        self.root_dir = root_dir   ### the csv path containing image list and mask list
-        self.dataframe = pd.read_csv(os.path.join(root_dir,split+'_fold'+fold_num+'.csv'))
-        self.split = split
-        self.task = task
-        #self.joint_transform = JointTransform(size=(image_size, image_size),apply_transform=add_transform)  # Example size
-        self.do_transform = add_transform
-        #no transform for segmentation task. This could be any additional image-specific transform
-        
+    def __init__(self,dataper, root_dir, image_size, fold_num, mode="train", add_transform=True):
+        self.root_dir = root_dir
+        if dataper!='':
+            self.dataframe = pd.read_csv(os.path.join(root_dir,dataper+'_'+mode+'_fold'+fold_num+'.csv'))
+        else:
+            self.dataframe = pd.read_csv(os.path.join(root_dir,mode+'_fold'+fold_num+'.csv'))
+        self.images = self.dataframe['image'].tolist()
+        self.labels = self.dataframe['label'].tolist()
         self.image_size = image_size
-        self.images = dataframe['filename'].tolist()
-        self.labels = dataframe['label'].tolist()
-        #self.images_dir = os.path.join(root_dir, split, "img")
-        #self.masks_dir = os.path.join(root_dir, split, "msk")
-        self.to_tensor = transforms.ToTensor() 
-        #self.images = sorted(os.listdir(self.images_dir))
-        self.transform = transforms.Compose([
-            transforms.Resize((image_size, image_size)),  # Resize to a fixed size
+        self.do_transform = add_transform
+
+        # Common transforms
+        self.common_transforms = transforms.Compose([
+            transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),
-            # Include normalization if required
+            # Add any normalization if required
         ])
-        
+
+        # Augmentation transforms
+        self.augmentation_transforms = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(20),
+            transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), shear=0.1),
+            transforms.Resize((int(image_size * 1.1), int(image_size * 1.1))),
+            transforms.RandomCrop(image_size),
+        ])
+
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_name = self.images[idx] #]os.path.join(self.images_dir, self.images[idx])
-        label = convert_label(self.labels[idx]) 
+        img_name = self.images[idx]
+        label = convert_label(self.labels[idx])
+        image = Image.open(img_name).convert("RGB")
 
-        image = Image.open(img_name).convert("RGB")   
-        image = TF.resize(image, self.image_size)
-        image = np.asarray(image)
-        
-        if self.do_transform == True:
-            if random.random() > 0.5:
-                k = np.random.randint(0, 4)
-                image = np.rot90(image, k)
-                axis = np.random.randint(0, 2)
-                image = np.flip(image, axis=axis).copy()
-            elif random.random() > 0.5:
-                angle = np.random.randint(-20, 20)
-                image = ndimage.rotate(image, angle, order=0, reshape=False)
-        # Convert the thresholded mask back to a tensor
-        image = (image - image.min())/(image.max()-image.min()+1e-6)
-        image = (image*255).astype(np.uint8)
-        image= Image.fromarray(image)
-        
-        image = self.transform(image)
-        
-        #image = torch.tensor(np.asarray(image), dtype=torch.float32).permute(2, 0, 1)
-        
-        # Convert label to a tensor
+        if self.do_transform:
+            image = self.augmentation_transforms(image)
+        image = self.common_transforms(image)
+
         label = torch.tensor(label, dtype=torch.long)
-        
-        return image, label, img_name
-        
+        return image, label, img_name        
         
 def main(args):
-    utils_f.init_distributed_mode(args)
+    if args.distributed:
+        utils_f.init_distributed_mode(args)
+    
+        print(args)
+    
+        device = torch.device(args.device)
+    
+        # fix the seed for reproducibility
+        seed = args.seed + utils_f.get_rank()
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        # random.seed(seed)
+    
+        cudnn.benchmark = True
+    else:
+        seed = args.seed + utils_f.get_rank()
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        device = torch.device(args.device)
+        print(args)
 
-    print(args)
-
-    device = torch.device(args.device)
-
-    # fix the seed for reproducibility
-    seed = args.seed + utils_f.get_rank()
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    # random.seed(seed)
-
-    cudnn.benchmark = True
+    
         
     # Set model
     model = swin_class.__dict__['swin_class'](num_classes=args.num_classes)
@@ -228,12 +231,14 @@ def main(args):
 
     # Set dataset
     #dataset_train = datasets.ImageFolder(args.data_path, transform=transform_train)
-    dataset_train = ClassificationCSVDataset(root_dir=args.data_path,image_size = args.input_size, mode="train" ,add_transform=args.transform)
+    dataset_train = ClassificationCSVDataset(dataper = args.datapercent,root_dir=args.data_path,image_size = args.input_size,fold_num=args.fold_num, mode="train" ,add_transform=args.transform)
     if args.eval == True:
-        dataset_val = ClassificationCSVDataset(root_dir=args.data_path,image_size = args.input_size, mode="test" ,add_transform=False)
+        dataset_val = ClassificationCSVDataset(dataper = '',root_dir=args.data_path,image_size = args.input_size,fold_num=args.fold_num, mode="test" ,add_transform=False)
     else:
-        dataset_val = ClassificationCSVDataset(root_dir=args.data_path,image_size = args.input_size, mode="val" ,add_transform=False)
-    if True:  # args.distributed:
+        dataset_val = ClassificationCSVDataset(dataper='',root_dir=args.data_path,image_size = args.input_size,fold_num=args.fold_num, mode="val" ,add_transform=False)
+    
+    
+    if  args.distributed:
         num_tasks = utils_f.get_world_size()
         global_rank = utils_f.get_rank()
         sampler_rank = global_rank
@@ -265,20 +270,42 @@ def main(args):
         )
     
     # Log output
+
+    status = 'use_pretrain'
+    study_name = args.output_dir +'/'+args.task+'/'+status
     
-    if global_rank == 0 and args.log_dir is not None:
-        os.makedirs(args.log_dir, exist_ok=True)
-        log_writer = SummaryWriter(logdir=args.log_dir)
+    if args.eval==True:
+        study_name = args.output_dir +'/'+args.task+'/'+ args.finetune.split('/')[-2]
+    if args.distributed==True and global_rank == 0:
+        os.makedirs(study_name, exist_ok=True)
+    
+    
+    if args.eval!=True:
+        with open(os.path.join(study_name, "running_param.txt"), 'w') as file:
+            json.dump(str(args), file)
+    if args.distributed==True and global_rank == 0 and study_name is not None:
+        os.makedirs(study_name, exist_ok=True)
+        log_writer = SummaryWriter(logdir=study_name)
         #log_writer = utils.TensorboardLogger(log_dir=args.log_dir)
+    elif args.distributed!=True and study_name is not None:
+        os.makedirs(study_name, exist_ok=True)
+        log_writer = SummaryWriter(logdir=study_name)
     else:
         log_writer = None
     
+
     model.to(device)
     model_without_ddp = model
 
     # Set optimizer
     param_groups = [p for p in model_without_ddp.parameters() if p.requires_grad]
-    optimizer = torch.optim.Adam(param_groups, lr=args.lr)#, weight_decay=5e-2, betas=(0.9, 0.95))  # ???5E-2
+    #optimizer = torch.optim.Adam(param_groups, lr=args.lr)#, weight_decay=5e-2, betas=(0.9, 0.95))  # ???5E-2
+    if args.optimizer == 'adamw':
+        optimizer = torch.optim.AdamW(param_groups, lr=args.lr, weight_decay=5e-2, betas=(0.9, 0.95))  # ???5E-2
+    if args.optimizer == 'sgd':
+        optimizer = torch.optim.SGD(param_groups, lr=args.lr, momentum=0.9, weight_decay=0.0001)
+    if args.optimizer == 'adam':
+        optimizer = torch.optim.Adam(param_groups, lr=args.lr, eps= 1e-06, weight_decay=0.1)
     #optimizer = torch.optim.SGD(param_groups, lr=args.lr, momentum=0.9, weight_decay=0.0001)
     loss_scaler = NativeScaler()
     criterion = torch.nn.CrossEntropyLoss()
@@ -290,7 +317,7 @@ def main(args):
     
     ### test evaluation
     if args.eval:
-        test_stats = evaluate(data_loader_val, model, device, args.num_classes, args.output_dir)
+        test_stats = evaluate(data_loader_val, model, device, args.num_classes, study_name)
         #print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['dice_score']:.3f}")
         exit(0)
     
@@ -303,10 +330,10 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir and ((epoch + 1) % args.save_freq == 0 or epoch + 1 == args.epochs):
-            misc.save_model(
-                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                loss_scaler=loss_scaler, epoch=epoch + 1)
+        #if args.output_dir and ((epoch + 1) % args.save_freq == 0 or epoch + 1 == args.epochs):
+        #    misc.save_model(
+        #        args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+        #        loss_scaler=loss_scaler, epoch=epoch + 1)
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
                      'epoch': epoch, }
@@ -320,7 +347,7 @@ def main(args):
                 
                 misc.save_model(
                         args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                        loss_scaler=loss_scaler, epoch="best")
+                        loss_scaler=loss_scaler,study_name=study_name, epoch="best")
 
             print(f'Min val loss: {min_loss:.3f}')
 
@@ -332,13 +359,21 @@ def main(args):
         if args.output_dir and misc.is_main_process():
             if log_writer is not None:
                 log_writer.flush()
-            with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
+            with open(os.path.join(study_name, "log.txt"), mode="a", encoding="utf-8") as f:
                 f.write(json.dumps(log_stats) + "\n")
+
 
 
 if __name__ == '__main__':
     arg = get_args_parser()
     arg = arg.parse_args()
+
+    status = 'use_pretrain'
+    study_name = arg.output_dir +'/'+arg.task+'/'+status
+    
+    if arg.eval==True:
+        study_name =arg.output_dir +'/'+arg.task+'/'+ arg.finetune.split('/')[-2]
+    
     if arg.output_dir:
-        Path(arg.output_dir).mkdir(parents=True, exist_ok=True)
+        Path(study_name).mkdir(parents=True, exist_ok=True)
     main(arg)
